2b2ba8cd097886fb50973e85e4d5643ee95c480e
George Nicola <george.nicola@softwareandpeople.ro>
2021-12-13 01:12:10 +0200

    code

--- src/components/Wheel/Wheel.js
+++ src/components/Wheel/Wheel.js
@@ -16,7 +16,6 @@ let tickSound = 'https://github.com/GeorgeNicola/react-component--wheels/blob/ma
 
 //Components
 import MessagesContainer from '../MessagesContainer/MessagesContainer';
-
 import { CONFIG } from '../../common/configuration';
 
 
@@ -27,7 +26,6 @@ function Wheel({params}) {
     const wheel = useRef(null);
     const tickTrigger = new Audio(tickSound);
     
-    
     useEffect(function(){
         //Removes the ticker's animation when it ends, so it can be triggered again
         document.querySelector(".ticker").addEventListener("animationend", function () {
@@ -35,30 +33,27 @@ function Wheel({params}) {
         })
     }, [])
 
-    const tick = () => {
-        console.log("Tick");
-        tickTrigger.play()
-        document.querySelector(".ticker").classList.add("ticker-animation")
+    const getCurrentRotation = (el) => {
+        var st = window.getComputedStyle(el, null);
+        var tm = st.getPropertyValue("-webkit-transform") ||
+                 st.getPropertyValue("-moz-transform") ||
+                 st.getPropertyValue("-ms-transform") ||
+                 st.getPropertyValue("-o-transform") ||
+                 st.getPropertyValue("transform") ||
+                 "none";
+        if (tm != "none") {
+          var values = tm.split('(')[1].split(')')[0].split(',');
+          /*
+          a = values[0];
+          b = values[1];
+          angle = Math.round(Math.atan2(b,a) * (180/Math.PI));
+          */
+          //return Math.round(Math.atan2(values[1],values[0]) * (180/Math.PI)); //this would return negative values the OP doesn't wants so it got commented and the next lines of code added
+          var angle = Math.round(Math.atan2(values[1],values[0]) * (180/Math.PI));
+          return (angle < 0 ? angle + 360 : angle); //adding 360 degrees here when angle < 0 is equivalent to adding (2 * Math.PI) radians before
+        }
+        return 0;
     }
-
-    const requestClaim = () => {
-        setLoaded(false)
-
-        setTimeout(function(){
-            setLoaded(true)
-            startWheel2()
-        }, 1000)
-    }
-
-    const play = () => { 
-        requestClaim() 
-    }
-
-
-
-
-
-
 
     const startWheel = () => {
         let deg = 720; //Rotate the wheel at least 2 times
@@ -100,28 +95,6 @@ function Wheel({params}) {
         }, 7.5);
     }
 
-    const getCurrentRotation = (el) => {
-        var st = window.getComputedStyle(el, null);
-        var tm = st.getPropertyValue("-webkit-transform") ||
-                 st.getPropertyValue("-moz-transform") ||
-                 st.getPropertyValue("-ms-transform") ||
-                 st.getPropertyValue("-o-transform") ||
-                 st.getPropertyValue("transform") ||
-                 "none";
-        if (tm != "none") {
-          var values = tm.split('(')[1].split(')')[0].split(',');
-          /*
-          a = values[0];
-          b = values[1];
-          angle = Math.round(Math.atan2(b,a) * (180/Math.PI));
-          */
-          //return Math.round(Math.atan2(values[1],values[0]) * (180/Math.PI)); //this would return negative values the OP doesn't wants so it got commented and the next lines of code added
-          var angle = Math.round(Math.atan2(values[1],values[0]) * (180/Math.PI));
-          return (angle < 0 ? angle + 360 : angle); //adding 360 degrees here when angle < 0 is equivalent to adding (2 * Math.PI) radians before
-        }
-        return 0;
-      }
-
     const startWheel3 = () => {
         //TO DO: initiate css animation
         //Listen for the position of the wheel (setInterval)
@@ -155,32 +128,51 @@ function Wheel({params}) {
 
     }
 
-    // const startWheel4 = () => {
-    //     let stopDeg = 720;
-    //     let tickRange = 360/PRIZES_COUNTER;
+    const startWheel4 = () => {
+        let stopDeg = 720;
+        let tickRange = 360/PRIZES_COUNTER;
 
 
 
-    //     const anim = anime({
-    //         targets: wheel.current,
-    //         rotate: '720deg',
-    //         duration: 5000,
-    //         easing: "easeOutQuad",
-    //         update: function(anim) {
-    //             let currentDeg = anim.progress*720/100;
-    //             console.log(Math.round(currentDeg % tickRange))
-    //             console.log(tickRange/2 - 3)
+        const anim = anime({
+            targets: wheel.current,
+            rotate: '720deg',
+            duration: 5000,
+            easing: "easeOutQuad",
+            update: function(anim) {
+                let currentDeg = anim.progress*720/100;
+                console.log(Math.round(currentDeg % tickRange))
+                console.log(tickRange/2 - 3)
 
-    //             if(Math.round(currentDeg % tickRange) === tickRange/2 - 3){
-    //                 console.log("TICK")
-    //                 tick();
-    //             }
+                if(Math.round(currentDeg % tickRange) === tickRange/2 - 3){
+                    console.log("TICK")
+                    tick();
+                }
 
-    //           }
-    //     });
+              }
+        });
     
 
-    // }
+    }
+
+    const tick = () => {
+        console.log("Tick");
+        tickTrigger.play()
+        document.querySelector(".ticker").classList.add("ticker-animation")
+    }
+
+    const requestClaim = () => {
+        setLoaded(false)
+
+        setTimeout(function(){
+            setLoaded(true)
+            startWheel2()
+        }, 1000)
+    }
+
+    const play = () => { 
+        requestClaim() 
+    }
 
     return(
         <div className="wheel-container">
